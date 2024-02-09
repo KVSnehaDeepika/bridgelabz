509a933cc9096fd288b2e264dfe85184025b05fd
KVSnehaDeepika <155156730+KVSnehaDeepika@users.noreply.github.com>
2024-02-09 11:59:21 +0530

    Update login.cs

--- login.cs
+++ login.cs
@@ -2,11 +2,11 @@ using System;
 namespace A{
 class login{
 public static void Main(){
-Console.WriteLine("Enter user name");
-             string euname=Console.ReadLine();
-             Console.WriteLine("Enter password");
-             string epassword=Console.ReadLine();
-             string uname = "deepika";
+    Console.WriteLine("Enter user name");
+    string euname=Console.ReadLine();
+    Console.WriteLine("Enter password");
+    string epassword=Console.ReadLine();
+    string uname = "deepika";
              string password = "d";
              if(uname==euname && epassword == password)
              {
